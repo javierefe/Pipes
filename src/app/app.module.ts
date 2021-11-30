499ce32a3cfd2bc244be847697d225cf633892a2
HUAWEI <jcfloresq@gmail.com>
2021-11-30 11:27:19 -0500

    huawei

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// primeNG
+import {ButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
